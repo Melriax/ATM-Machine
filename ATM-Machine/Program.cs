54115f54976d42f7e59824b9b348485f2139b1c7
Melriax <danfreimann@pm.me>
2021-02-06 21:12:48 +0100

    Added information in the main class

---
+++ ATM-Machine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ATM_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * The ATM-Machine programm - OOP
+             * The user can choose between the following 4 things:
+             * - check balance
+             * - get money
+             * - store money
+             * - send money
+             * the user has to have the option to finish the task after every step they make
+             */
+            
+            
+        }
+    }
+}
